d3654fb6a5aa6d04c147b42725faa38ec8d6154c
Edondjiibolo <enzombul@gmail.com>
2024-02-17 09:47:21 +0800

    archivo js servidor

---
+++ Server/server.mjs
@@ -0,0 +1,21 @@
+import express from "express";
+import { fileURLToPath } from "url";
+import { dirname, join } from "path";
+
+const app = express();
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+// Configura la carpeta "dist" como carpeta estática
+app.use(express.static(join(__dirname, "dist")));
+
+// Redirige todas las rutas a index.html
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "build", "index.html"));
+});
+
+// Inicia el servidor en el puerto 8080
+const PORT = 8080;
+app.listen(PORT, () => {
+  console.log(`El servidor está ejecutándose en el puerto ${PORT}`);
+});
