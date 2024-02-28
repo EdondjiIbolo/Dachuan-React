15bb780b848aaba8a9717ca7af0f5f3a7640adaf
Edondjiibolo <enzombul@gmail.com>
2024-02-28 17:29:38 +0800

    last update

--- src/components/pages/Buy Pages/CustomQuotation.jsx
+++ src/components/pages/Buy Pages/CustomQuotation.jsx
@@ -1,25 +1,28 @@
+import { UploadFileIcon, UploadIcon } from "../../Icons";
+import "../../sections/quotations.css";
+
 export function CustomQuotation() {
   return (
     <main className="pt-20  min-h-screen main-body">
-      <header className="h-16 flex items-center  bg-white shadow-md  ">
-        <section className="flex w-full items-center justify-between max-w-[1310px] m-auto">
+      <header className="sm:h-16 h-24 flex items-center  bg-white shadow-md  ">
+        <article className="flex flex-col gap-3 sm:flex-row w-full items-center justify-between max-w-[1310px] m-auto">
           <section className="flex gap-4 items-center ">
-            <p>
+            <p className="sm:text-base text-xs">
               <b>Quote Name :</b> Name
             </p>
-            <p>
+            <p className="sm:text-base text-xs">
               <b>Quote No: SQ000125555669</b>
             </p>
           </section>
           <button className="p-2 px-2 w-44 bg-blue-200 rounded-md hover:bg-blue-400 transition-all duration-150 ease-linear hover:scale-x-[1.02] hover:text-white text blue-800">
             Request for quotation
           </button>
-        </section>
+        </article>
       </header>
-      <main className="sm:px-32 py-5 flex gap-16 flex-col sm:flex-row">
+      <main className="md:px-32 px-2 py-5 flex gap-16 flex-col sm:flex-row">
         <section className="sm:w-2/3 flex flex-col  gap-3">
           <section className="  shadow rounded flex flex-col gap-3 bg-white p-3 ">
-            <header className="flex border-b-2 pb-2 justify-between items-center">
+            <header className="flex sm:flex-row flex-col gap-2 border-b-2 pb-2 justify-between items-center">
               <h2 className="font-semibold">Parts & Configurations</h2>
               <div className="flex gap-3">
                 <button className="bg-blue-300 p-1 rounded text-slate-800 px-2 font-semibold">
@@ -88,7 +91,112 @@ export function CustomQuotation() {
             <header className="flex border-b-2 pb-2 justify-between items-center">
               <h2 className="font-semibold"> Configurations</h2>
             </header>
-            <section className="flex gap-3 justify-between items-start "></section>
+            <form className="flex gap-5 flex-col pl-5">
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Quantity</p>
+                <input
+                  type="number"
+                  inputMode="number"
+                  defaultValue={1}
+                  className="bg-slate-200 w-72 p-1 rounded border outline-none border-gray-400"
+                />
+              </label>
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Material</p>
+                <select
+                  name="Maetrial"
+                  className="p-1 w-72 border rounded border-gray-500 outline-none"
+                >
+                  <option value="Aluminium 6061-T6">Aluminium 6061-T6</option>
+                  <option value="Aluminium 6061-T6">Aluminium 6061-T6</option>
+                  <option value="Aluminium 6061-T6">Aluminium 6061-T6</option>
+                  <option value="Aluminium 6061-T6">Aluminium 6061-T6</option>
+                  <option value="Aluminium 6061-T6">Aluminium 6061-T6</option>
+                </select>
+              </label>
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Finishing</p>
+                <select
+                  name="Finishing"
+                  className="p-1 w-72 border rounded border-gray-500 outline-none"
+                >
+                  <option value="Conductive Anodizing Type II">
+                    Conductive Anodizing Type II
+                  </option>
+                  <option value="Conductive Anodizing Type II">
+                    Conductive Anodizing Type II
+                  </option>
+                  <option value="Conductive Anodizing Type II">
+                    Conductive Anodizing Type II
+                  </option>
+                  <option value="Conductive Anodizing Type II">
+                    Conductive Anodizing Type II
+                  </option>
+                  <option value="Conductive Anodizing Type II">
+                    Conductive Anodizing Type II
+                  </option>
+                </select>
+              </label>
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Tolerance</p>
+                <select
+                  name="Finishing"
+                  className="p-1 w-72 border rounded border-gray-500 outline-none"
+                >
+                  <option value=" ISO 2768 Medium (m) ">
+                    ISO 2768 Medium (m)
+                  </option>
+                  <option value=" ISO 2768 Medium (m) ">
+                    ISO 2768 Medium (m)
+                  </option>
+                  <option value=" ISO 2768 Medium (m) ">
+                    ISO 2768 Medium (m)
+                  </option>
+                  <option value=" ISO 2768 Medium (m) ">
+                    ISO 2768 Medium (m)
+                  </option>
+                  <option value=" ISO 2768 Medium (m) ">
+                    ISO 2768 Medium (m)
+                  </option>
+                </select>
+              </label>
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Roughness</p>
+                <select
+                  name="Roughness"
+                  className="p-1 w-72 border rounded border-gray-500 outline-none"
+                >
+                  Ra1.6μm
+                  <option value="Ra1.6μm">Ra1.6μm</option>
+                  <option value="Ra1.6μm">Ra1.6μm</option>
+                  <option value="Ra1.6μm">Ra1.6μm</option>
+                  <option value="Ra1.6μm">Ra1.6μm</option>
+                  <option value="Ra1.6μm">Ra1.6μm</option>
+                </select>
+              </label>
+              <label className="flex flex-col gap-1">
+                <p className="font-semibold">Threads</p>
+                <select
+                  name="Threads"
+                  className="p-1 w-72 border rounded border-gray-500 outline-none"
+                >
+                  <option value="NO">NO</option>
+                  <option value="Yes, as 2D Technical Drawing">
+                    Yes, as 2D Technical Drawing
+                  </option>
+                </select>
+              </label>
+              <div>
+                <p className="font-bold">*2D Technical Drawing</p>
+                <label className="flex flex-col gap-1 cursor-pointer hover:border-2 transition-all duration-150 ease-linear p-3 h-32 border border-dashed border-blue-600 w-72 justify-center rounded items-center">
+                  <input type="file" className="hidden" />
+                  <UploadIcon />
+                  <p className="font-medium text-zinc-600">
+                    Upload your 2D file
+                  </p>
+                </label>
+              </div>
+            </form>
           </section>
         </section>
         <section className="sm:w-1/3 flex flex-col gap-3">
@@ -122,6 +230,11 @@ export function CustomQuotation() {
               <p>ahoiuhushisah</p>
             </article>
           </article>
+          <section className="flex-grow bg-white rounded shadow p-2 px-4">
+            <picture>
+              <div className="w-full aspect-square rounded bg-zinc-500"></div>
+            </picture>
+          </section>
         </section>
       </main>
     </main>
