e9c0ffba277f031a15acb62769dd67d6b2a676bd
Edondjiibolo <enzombul@gmail.com>
2024-03-12 10:28:13 +0800

    responsive table

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -25,8 +25,8 @@ export function Header() {
   };
   return (
     <header className="main-header  justify-center h-20  bg-white w-full flex fixed top-0 left-0 z-40  md:pr-5">
-      <section className="flex w-full max-w-[1320px]  items-center  border-b-2  bg-white  m-auto justify-between  h-20 z-30  fixed   ">
-        <section className="header__container--right pl-2">
+      <section className="flex w-full max-w-[1320px]  items-center  border-b-2   m-auto justify-between  h-20 z-30  fixed  ">
+        <section className="header__container--right px-2">
           <a
             href="/"
             className="w-44 h-20  flex items-center overflow-hidden"
@@ -40,7 +40,7 @@ export function Header() {
           </a>
         </section>
         <section className={className} id="menu">
-          <ul className="menu sm:flex-1 ">
+          <ul className="menu sm:flex-1 gap-2">
             {sections.map((section, index) => {
               return (
                 <li key={index} className="menu__item">
